a36516bb4fff45caff3e5d19de1895c572b388e2
kimaniwalker <kimaniwalker@gmail.com>
2023-02-07 20:31:22 -0600

    [feat]-new app version

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default () => {
             name: "KC CANDLE CO",
             owner: "kimaniwalker",
             slug: "kcmobile",
-            version: "1.0.1",
+            version: "1.0.2",
             orientation: "portrait",
             icon: "./assets/KCICON.png",
             userInterfaceStyle: "light",
